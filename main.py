29f5d112ef15800c1227fc51d59d4ab86508c3e7
Praveen raj kumar Vaddadi <praveenvaddadi15@gmail.com>
2026-02-09 10:51:18 +0530

    Fix static files mount path in FastAPI app

--- main.py
+++ main.py
@@ -42,7 +42,7 @@ interpreter = Interpreter(model_path="accident_detection_model.tflite")
 # ---------------- FASTAPI ----------------
 app = FastAPI()
 templates = Jinja2Templates(directory="templates")
-app.mount("/static", StaticFiles(directory="static"), name="static")
+app.mount("./static", StaticFiles(directory="static"), name="static")
 
 
 @app.get("/", response_class=HTMLResponse)
